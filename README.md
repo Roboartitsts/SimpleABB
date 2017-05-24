95e8010b73f4103ab9f3c2e809d579c8100ab3fd
Christopher Budo <budocf@rose-hulman.edu>
2017-05-24 16:10:50 -0400

    additions to readme

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ A simple python class to connect to the ABB robot arm at Rose-Hulman.
 The functions in this library will allow you to easily send movements commands to the Rose-Hulman ABB Arm through a serial connection.
 
 ## Usage
-To use this library, you will need to have python installed on your computer, this library should be compatible with python 3.x. To check the version of python you are running, use ```python --version``` in your command line.
+To use this library, you will need to have python installed on your computer, this library has been tested with python 3.x. To check the version of python you are running, use ```python --version``` in your command line.
 
-In order to use the functions made available in this library, add the ```abbsimple.py``` file to your project directory and type ```from abbsimple import ABBRunner``` at the top of your python file.
+This library uses serial communication for sending commands to the robot.  In order to use this, it is necessary to add the serial library to python.  To install the serial library, in your terminal use the command ```pip install pyserial``` or add pyserial as a dependency in your IDE.
+
+In order to use the functions made available in this library, add the [abbsimple.py](/abbsimple.py) file to your project directory and type ```from abbsimple import ABBRunner``` at the top of your python file.
+
+#### TL;DR
+1. Use Python 3
+2. ```pip install pyserial```
+3. ```from abbsimple import ABBRunner```
+4. ```rob = ABBRunner()```
--- abbsimple.py
+++ abbsimple.py
@@ -2,112 +2,181 @@ import serial
 import time
 class ABBRunner():
 
-	def __init__(self, width=2530, height=2530):
-		self.ser = None
-		self.connected = False
-		self.width = width
-		self.height = height
+    def __init__(self, width=2530, height=2530):
+        self.ser = None
+        self.connected = False
+        self.width = width
+        self.height = height
 
-	def moveTo(self, x, y):
-		if not self.connected:
-			return False
+    def moveTo(self, x, y):
+        """
+        Moves the robot arm to the specified (x,y) position on the canvas.
 
-		msg = "COORDM:X:" + str(x) + ",Y:" + str(y) + ";"
-		return self.sendSerial(msg)
+        Parameters
+        ----------
+        x : int
+            X Position of the destination point
 
-	def sendBezierPoints(self, x1, y1, x2, y2 ,x3, y3):
-		if not self.connected:
-			return False
+        y : int
+            Y Position of the destination point
 
-		msg = "COORDQ:X:{:.3f},Y:{:.3f}#X:{:.3f},Y:{:.3f}#X:{:.3f},Y:{:.3f};".format(x1,y1,x2,y2,x3,y3)
-		return self.sendSerial(msg)
+        """
+        if not self.connected:
+            return False
 
-	def gripToggle(self):
-		''' By default toggles if '''
-		msg = "GRIP;"
-		return self.sendSerial(msg)
+        msg = "COORDM:X:" + str(x) + ",Y:" + str(y) + ";"
+        return self.sendSerial(msg)
 
-	def brushUp(self):
-		''' By default toggles if '''
-		msg = "BRUSHUP;"
-		return self.sendSerial(msg)
+    def sendBezierPoints(self, x1, y1, x2, y2 ,x3, y3):
+        """
+        Moves the robot arm along the specified Bezier Curve.
 
-	def brushDown(self):
-		''' By default toggles if '''
-		msg = "BRUSHDOWN;"
-		return self.sendSerial(msg)
+        Parameters
+        ----------
+        x : int
+            X Position of the destination point
 
-	def grip(self, toSqueeze):
-		if toSqueeze:
-			msg = "GRIPV" + str(toSqueeze)+ ";"
-		return self.sendSerial(msg)
+        y : int
+            Y Position of the destination point
 
-	def moveToSafe(self,):
-		if not self.connected:
-			return False
-		return self.sendSerial("MOVETOSAFE;")
+        """
+        if not self.connected:
+            return False
 
-	def sendCanvasInfo(self):
-		if not self.connected:
-			return False
+        msg = "COORDQ:X:{:.3f},Y:{:.3f}#X:{:.3f},Y:{:.3f}#X:{:.3f},Y:{:.3f};".format(x1, y1, x2, y2, x3, y3)
+        return self.sendSerial(msg)
 
-		# Wait for robot to be ready
-		if not self.waitRobotReady():
-			return False
-		# self.ser.timeout = None
+    def gripToggle(self):
+        """
+        Toggles the ABB Gripper
+        """
+        msg = "GRIP;"
+        return self.sendSerial(msg)
 
-		msg = "SIZE:X:" + str(self.width) + ",Y:" + str(self.height) + ";"
-		self.sendSerial(msg)
-		something = self.readSerial()
-		self.moveToSafe()
-		return something
+    def brushUp(self):
+        """
+        Takes the Brush off the canvas
+        """
+        msg = "BRUSHUP;"
+        return self.sendSerial(msg)
 
-	def setSize(self, width, height):
-		self.width = width
-		self.height = height
+    def brushDown(self):
+        """
+        Places the Brush on the canvas
+        """
+        msg = "BRUSHDOWN;"
+        return self.sendSerial(msg)
 
-	def waitRobotReady(self):
-		while True:
-			code = self.readSerialLine()
-			print("waiting on robot...")
-			if code:
-				print(code)
-				return True
+    def grip(self, toSqueeze):
+        """
+        Allows the user to specify the configuration of the gripper
 
-		return False
+        Parameters
+        ----------
+        toSqueeze : int
 
-	def abort(self,):
-		self.ser.close()
+            1 to grip
+            0 to release
 
-	def connectToSerial(self, port):
-		self.ser = serial.Serial(port, 115200, timeout=0.1)
-		self.connected = True
-		self.sendCanvasInfo()
+        """
+        if toSqueeze:
+            msg = "GRIPV" + str(toSqueeze)+ ";"
+        return self.sendSerial(msg)
 
-	def readSerial(self,):
-		msg = self.ser.read(2)
-		return msg
+    def moveToSafe(self,):
+        """
+        moves the ABB Arm to a safe position above the canvas
+        """
+        if not self.connected:
+            return False
+        return self.sendSerial("MOVETOSAFE;")
 
-	def readSerialLine(self,):
-		msg = self.ser.readline()
-		return msg
+    def sendCanvasInfo(self):
+        """
+        sends the canvas info to the abb arm
+        """
+        if not self.connected:
+            return False
 
-	def sendSerial(self, msg):
-		if self.connected == False:
-			return False
-		print(msg)
-		# TODO: Try/catch?
-		self.ser.write(msg.encode())
-                #print('waiting for response')
-		resp = self.ser.read(1)
-		# print("received command:",resp)
-		#print('---respfast----')
-                #if resp != '':
-                #    print(resp,ord(resp))
-		#print('---respfast----')
-		while resp == '':
-			time.sleep(0.1)
-			resp = self.ser.read(1)
-			if ord(resp) == 2:
-    				return self.sendSerial(msg)
-		return True
+        # Wait for robot to be ready
+        if not self.waitRobotReady():
+            return False
+        # self.ser.timeout = None
+
+        msg = "SIZE:X:" + str(self.width) + ",Y:" + str(self.height) + ";"
+        self.sendSerial(msg)
+        something = self.readSerial()
+        self.moveToSafe()
+        return something
+
+    def setSize(self, width, height):
+        """
+        sets the size of the drawing area available to the robot
+        """
+        self.width = width
+        self.height = height
+
+    def waitRobotReady(self):
+        """
+        waits until the ABB Arm is able to receive serial communication
+        """
+        while True:
+            code = self.readSerialLine()
+            print("waiting on robot...")
+            if code:
+                print(code)
+                return True
+
+        return False
+
+    def abort(self,):
+        """
+        closes serial communication
+        """
+        self.ser.close()
+
+    def connectToSerial(self, port):
+        """
+        connects to the specified serial port
+        
+        Parameters
+        ----------
+        port : str
+
+            The com port that the robot is connected to
+            On a Mac/Unix machine this will be of the form '/dev/<usbSerialdevice>'
+            On a Windows machine this will be 'COM#'
+        """
+        self.ser = serial.Serial(port, 115200, timeout=0.1)
+        self.connected = True
+        self.sendCanvasInfo()
+
+    def readSerial(self,):
+        """
+        reads a serial message from the ABB Arm
+        """
+        msg = self.ser.read(2)
+        return msg
+
+    def readSerialLine(self,):
+        """
+        Reads a line of serial communication from the ABB Arm
+        """
+        msg = self.ser.readline()
+        return msg
+
+    def sendSerial(self, msg):
+        """
+        Sends a command to the robot over the serial connection
+        """
+        if not self.connected:
+            return False
+        print(msg)
+        self.ser.write(msg.encode())
+        resp = self.ser.read(1)
+        while resp == '':
+            time.sleep(0.1)
+            resp = self.ser.read(1)
+            if ord(resp) == 2:
+                return self.sendSerial(msg)
+        return True
